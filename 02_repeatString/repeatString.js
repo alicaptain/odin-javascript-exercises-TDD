002ff1cc814b85767797d6fabcc4798336fd8e4f
alicaptain <aligulerworks@gmail.com>
2022-04-25 20:25:18 +0300

    Proceed Studying

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function (singleString, theNum) {
+   let allStrings = '';
+   if (theNum < 0) {
+      allStrings = 'ERROR';
+   } else {
+      for (i = 0; i < theNum; i++) {
+
+         allStrings = allStrings + singleString;
+
+      }
+   }
+   return allStrings;
+
 
 };
+
+console.log(typeof singleString);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function (toBeReversed) {
+   let arrayed = toBeReversed.split('');
+   let reversedArray = arrayed.reverse();
+   return reversedArray.join('');
 
+   console.log(toBeReversed);
+   console.log(arrayed);
+   console.log(reversedArray);
 };
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
