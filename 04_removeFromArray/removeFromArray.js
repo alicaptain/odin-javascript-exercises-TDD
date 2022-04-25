a1bf6d7414be4bb9adb992ef6131bedb6d228b60
alicaptain <aligulerworks@gmail.com>
2022-04-25 20:23:56 +0300

    Proceed Studying

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (incomingArray, ...incomingWhatEver) {
 
-};
+   countRest: for (oneWhatEver of incomingWhatEver) {
+      let usualSuspectIndex = incomingArray.findIndex((catchSuspect) => catchSuspect === oneWhatEver);
+      if (usualSuspectIndex !== -1) {
+         incomingArray.splice(usualSuspectIndex, 1)
+      };
+   }
+   return incomingArray;
+}
+
+
+
+
+;
 
 // Do not edit below this line
 module.exports = removeFromArray;
