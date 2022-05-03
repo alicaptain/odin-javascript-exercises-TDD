296feb421b681116fb980c0487fe41a1f01bd8af
alicaptain <aligulerworks@gmail.com>
2022-05-03 14:48:46 +0300

    Finish another part of the course: sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function (firstInt, secondInt) {
 
+   if (firstInt < 0 || secondInt < 0) {
+      return 'ERROR';
+   } else if (typeof firstInt !== 'number' || typeof secondInt !== 'number') {
+      return 'ERROR';
+   }
+
+   let smallNum = Math.min(firstInt, secondInt);
+   let bigNum = Math.max(firstInt, secondInt);
+   let count = Math.abs(firstInt - secondInt);
+   let resultNum = 0;
+
+
+
+   while (smallNum <= bigNum) {
+      resultNum = resultNum + smallNum;
+      smallNum++;
+
+   }
+   return resultNum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
