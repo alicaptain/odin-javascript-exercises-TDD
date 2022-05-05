2c010099b92f5ec07f48ac237c7b0ca3904f67c5
alicaptain <aligulerworks@gmail.com>
2022-05-05 13:16:40 +0300

    Another Quiz | tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,28 @@
-const ftoc = function() {
+const ftoc = function (fahrenheit) {
+  if (isNaN(fahrenheit)) {
+    return 'ERROR';
+  } else {
+    let rawCelsius = (fahrenheit - 32) / 1.8;
+    let Celsius = Math.round(rawCelsius * 10) / 10;
+    return Celsius;
+  }
+
+
 
 };
 
-const ctof = function() {
+const ctof = function (celsius) {
+  if (isNaN(celsius)) {
+    return 'ERROR';
+  } else {
+    let rawFahrenheit = (celsius * 1.8) + 32;
+    let Fahrenheit = Math.round(rawFahrenheit * 10) / 10;
+    return Fahrenheit;
+  }
 
 };
+
+
 
 // Do not edit below this line
 module.exports = {
