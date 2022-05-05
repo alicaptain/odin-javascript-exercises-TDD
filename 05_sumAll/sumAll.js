5a09dd140bc00c4a682132af366b2a639b5e3565
alicaptain <aligulerworks@gmail.com>
2022-05-05 11:15:10 +0300

    Just Another Step

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,25 +1,34 @@
-const sumAll = function (firstInt, secondInt) {
+// const sumAll = function (firstInt, secondInt) {
+// 
+//    if (firstInt < 0 || secondInt < 0) {
+//       return 'ERROR';
+//    } else if (typeof firstInt !== 'number' || typeof secondInt !== 'number') {
+//       return 'ERROR';
+//    }
+// 
+//    let smallNum = Math.min(firstInt, secondInt);
+//    let bigNum = Math.max(firstInt, secondInt);
+//    let count = Math.abs(firstInt - secondInt);
+//    let resultNum = 0;
+// 
+//    while (smallNum <= bigNum) {
+//       resultNum = resultNum + smallNum;
+//       smallNum++;
+// 
+//    }
+//    return resultNum;
+// };
 
-   if (firstInt < 0 || secondInt < 0) {
-      return 'ERROR';
-   } else if (typeof firstInt !== 'number' || typeof secondInt !== 'number') {
-      return 'ERROR';
-   }
 
-   let smallNum = Math.min(firstInt, secondInt);
-   let bigNum = Math.max(firstInt, secondInt);
-   let count = Math.abs(firstInt - secondInt);
-   let resultNum = 0;
+function sumAll(a, b) {
+   if (typeof a != 'number' || typeof b != 'number' || a < 0 || b < 0) return 'ERROR'
+
+   return Array.from({
+      length: Math.abs(a - b) + 1
+   }, (v, k) => k + Math.min(a, b)).reduce((a, v) => a + v)
+}
 
 
-
-   while (smallNum <= bigNum) {
-      resultNum = resultNum + smallNum;
-      smallNum++;
-
-   }
-   return resultNum;
-};
 
 
 // Do not edit below this line
